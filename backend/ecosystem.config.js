c2b51d303d166dab4991a4bcc70b705aeb332650
Роман Маскаев <subulka@mail.ru>
2024-08-11 23:30:47 +0300

    j

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -1,8 +1,24 @@
+require('dotenv').config({ path: __dirname + `/.env.deploy` });
+
+const { DEPLOY_USER, DEPLOY_HOST, DEPLOY_PATH, DEPLOY_REF, DEPLOY_REPO } =
+  process.env;
+
 module.exports = {
-    apps: [
-      {
-        name: 'app',
-        script: './dist/main.js',
-      },
-    ],
-  };
+  apps: [
+    {
+      name: 'app',
+      script: './dist/main.js',
+    },
+  ],
+  deploy: {
+    production: {
+      user: DEPLOY_USER,
+      host: DEPLOY_HOST,
+      ref: DEPLOY_REF,
+      repo: DEPLOY_REPO,
+      path: DEPLOY_PATH,
+      'pre-deploy-local': `bash scripts/deployEnv.sh ${DEPLOY_USER}@${DEPLOY_HOST} ${DEPLOY_PATH}`,
+      'post-deploy': 'sudo docker compose up -d --build',
+    },
+  },
+};
